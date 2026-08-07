6bd312742a774b937ba4c489cad4b819af9b5c64
Oleksandr_Kobzar <oleksandrkobzar@gmail.com>
2026-08-07 12:59:05 +0300

    Added auth.js into services and replaced authValidation Scheme

--- src/validations/articlesValidation.js
+++ src/validations/articlesValidation.js
@@ -1,9 +1,1 @@
-import { Joi, Segments } from 'celebrate';
 
-export const registerUserSchema = {
-  [Segments.BODY]: Joi.object({
-    name: Joi.string().min(2).max(32).required(),
-    email: Joi.string().email().max(64).required(),
-    password: Joi.string().min(8).max(64).required(),
-  }),
-};
