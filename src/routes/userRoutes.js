589531875502c48a8c9129865cef8857ac684712
Musiienko <mysiienko.vlad14062007@gmail.com>
2026-08-07 21:34:53 +0300

    fix language of controller

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
 import { authenticate } from '../middleware/authenticate.js';
-import { updateUserAvatar, getUserById } from '../controllers/userController.js';
+import { updateUserAvatar, getUserById, addSavedArticle, removeSavedArticle } from '../controllers/userController.js';
 import { getSavedArticles, getCreatedArticles, } from '../controllers/userController.js';
 import { upload } from "../middleware/multer.js";
 
 const router = Router();
 
+
+router.post('/saved', authenticate, addSavedArticle);
+router.delete('/saved', authenticate, removeSavedArticle);
 router.get('/saved', authenticate, getSavedArticles);
+
 router.get('/:userId/articles', getCreatedArticles);
 router.get('/:id', getUserById);
 
