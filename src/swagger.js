8b3f15b33e44884fbb368f9f2946ce414f8e0502
Taras Bilyi <taras.bilyi@gmail.com>
2026-08-11 14:34:04 +0300

    Update swagger.js

--- src/swagger.js
+++ src/swagger.js
@@ -16,7 +16,4 @@ const routes = [
   './routes/userRoutes.js',
 ];
 
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
-
 swaggerAutogen({ openapi: '3.0.0' })(outputFile, routes, doc);
